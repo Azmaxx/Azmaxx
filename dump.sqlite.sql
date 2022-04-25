e1ed7bcadb16d5bd7b0ebbabab234bf6e3d1543f
Omileye Afees <98825437+Azmaxx@users.noreply.github.com>
2022-04-25 14:24:33 +0100

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE 'SQLite' ('c1' INTEGER,'c2' TEXT,'c3' TEXT);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
